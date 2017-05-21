486d9c5dbcb11f7695d85201d3a11201dad244e0
maxwell92 <710028463@qq.com>
2017-05-21 14:22:44 +0800

    update main

--- src/main.go
+++ src/main.go
@@ -4,12 +4,14 @@ import (
 	"common/router"
 	"common/server"
 	"common/config"
+	"github.com/maxwell92/gokits/mysql"
 	"fmt"
 )
 
 func init() {
 	router.Setup()
 	config.Instance().Load()
+	mysql.NewMysqlClient(config.Instance().DbHost, config.Instance().DbPort, config.Instance().DbUser, config.Instance().DbPass, config.Instance().DbName, config.Instance().DbPool).Open()
 }
 
 func main() {
