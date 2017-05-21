7cc6f7750d220b09f67ded00d7d8727e189d3805
maxwell92 <710028463@qq.com>
2017-05-21 14:22:31 +0800

    add node get and post test

---
+++ src/common/config/constant.go
@@ -0,0 +1,17 @@
+package config
+
+const (
+	ENV_DBHOST = "DB_HOST"
+	ENV_DBPORT = "DB_PORT"
+	ENV_DBUSER = "DB_USER"
+	ENV_DBPASS = "DB_PASS"
+	ENV_DBNAME = "DB_NAME"
+	ENV_DBPOOL = "DB_POOL"
+
+	DB_HOST = "localhost"
+	DB_PORT = "3306"
+	DB_USER = "root"
+	DB_PASS = "root"
+	DB_NAME = "beehive"
+	DB_POOL = 10
+)
--- src/controller/node/node.go
+++ src/controller/node/node.go
@@ -9,5 +9,10 @@ type NodeController struct {
 }
 
 func (nc NodeController) Get() {
-
+	nc.WriteOk("node get ok")
 }
+
+func (nc NodeController) Post() {
+	nc.WriteOk("node post ok")
+}
+
