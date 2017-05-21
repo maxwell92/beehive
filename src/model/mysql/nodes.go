0dc747736ca0c3b4e3a360a4241e36f9c7d57e6c
maxwell92 <710028463@qq.com>
2017-05-21 14:23:05 +0800

    add nodes

--- src/model/mysql/nodes.go
+++ src/model/mysql/nodes.go
@@ -1,1 +1,25 @@
 package mysql
+
+import (
+
+)
+
+
+type Node interface {
+	Insert() error
+	Query() error
+}
+
+type NodeInfo struct {
+
+}
+
+func (ni *NodeInfo)Insert() error {
+
+}
+
+func (ni *NodeInfo)Query() error {
+
+}
+
+
